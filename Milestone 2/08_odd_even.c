46004cecfc05af048066eab2a454fdcf04d36154
Atharva Deshmukh <46042891+AtharvaDev@users.noreply.github.com>
2020-09-02 12:09:35 +0530

    Create 08_odd_even.c

---
+++ Milestone 2/08_odd_even.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	printf("Enter any noumber\n");
+	scanf("%d",&i);
+	{
+    if (i % 2==0){
+      printf("the number is even");
+   
+    }else{
+      printf("the number is odd");
+    }
+	}
+	
+	return 0;
+}
