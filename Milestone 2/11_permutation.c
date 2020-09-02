bfc1cd4aec350bfd8042f3c0ac778dbc60073494
Atharva Deshmukh <46042891+AtharvaDev@users.noreply.github.com>
2020-09-02 12:11:38 +0530

    Create 11_permutation.c

---
+++ Milestone 2/11_permutation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int Fact (int x)
+{
+if (x == 1)
+return 1;
+return (x * Fact(x-1));
+}
+
+int main()
+{
+	int n,r;
+	float per;
+	printf("Enter values for n and r :\n");
+	scanf("%d%d",&n,&r);
+	per=Fact(n)/Fact(n-r);
+	printf("Permutation = %.2f",per);
+	return 0;
+}
+
